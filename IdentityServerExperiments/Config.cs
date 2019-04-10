2faca21b42ea634e87e44917130503a9a32d7643
Gustavo Santos <gustavo.dev@outlook.com.br>
2019-04-10 20:25:56 -0300

    Add project files.

---
+++ IdentityServerExperiments/Config.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace IdentityServerExperiments
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("ResourceApi", "Resources api with no data.")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "test",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = 
+                    {
+                        new Secret("test".Sha256())
+                    },
+                    AllowedScopes = { "ResourceApi" }
+                }
+            };
+        }
+    }
+}
