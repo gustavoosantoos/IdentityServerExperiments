a5f63d6b6cec279fa7f6df087ac3bacd938ca902
Gustavo Santos <gustavo.dev@outlook.com.br>
2019-04-10 21:00:22 -0300

    Added resource api with authentication

---
+++ IdentityServerExperiments.ResourceApi/Startup.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using static IdentityModel.OidcConstants;
+
+namespace IdentityServerExperiments.ResourceApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(AuthenticationSchemes.AuthorizationHeaderBearer)
+                .AddIdentityServerAuthentication(options =>
+                {
+                    options.Authority = "http://localhost:5000";
+                    options.RequireHttpsMetadata = false;
+                    options.ApiName = "ResourceApi";
+                });
+
+            services.AddMvc()
+                .AddNewtonsoftJson();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseRouting(routes =>
+            {
+                routes.MapControllers();
+            });
+        }
+    }
+}
