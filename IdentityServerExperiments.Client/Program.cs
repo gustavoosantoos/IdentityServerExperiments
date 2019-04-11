a8c212cc5c3ec7a2454dbe39b8daa680d88eb271
Gustavo Santos <gustavo.dev@outlook.com.br>
2019-04-10 21:48:03 -0300

    Added client consuming resource from resourceapi with token

---
+++ IdentityServerExperiments.Client/Program.cs
@@ -0,0 +1,48 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace IdentityServerExperiments.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            using (var client = new HttpClient())
+            {
+                var discovery = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+                if (discovery.IsError)
+                {
+                    Console.WriteLine("Error while discovering authority:" + discovery.Error);
+                    Console.ReadKey();
+                    return;
+                }
+
+                var token = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+                {
+                    ClientId = "test",
+                    ClientSecret = "test",
+                    Address = discovery.TokenEndpoint
+                });
+
+                if (token.IsError)
+                {
+                    Console.WriteLine("Error while loading token: " + token.Error);
+                    Console.ReadKey();
+                    return;
+                }
+
+                Console.WriteLine(token.AccessToken);
+                Console.WriteLine("\n\n");
+
+                client.SetBearerToken(token.AccessToken);
+                var response = await client.GetStringAsync("http://localhost:7000/api/values");
+
+                Console.WriteLine(response);
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
