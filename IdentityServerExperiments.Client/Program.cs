4ec40740017c7f6aafd1a14fd1da422f499ae887
Gustavo Santos <gustavo.dev@outlook.com.br>
2019-04-10 23:30:22 -0300

    Getting Token with Password Grant Type

--- IdentityServerExperiments.Client/Program.cs
+++ IdentityServerExperiments.Client/Program.cs
@@ -9,6 +9,53 @@ namespace IdentityServerExperiments.Client
     {
         public static async Task Main(string[] args)
         {
+            await GetTokenWithClientCredentials();
+            await GetTokenWithResourceOwnerPassword();
+        }
+
+        private static async Task GetTokenWithResourceOwnerPassword()
+        {
+            using (var client = new HttpClient())
+            {
+                var discovery = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+                if (discovery.IsError)
+                {
+                    Console.WriteLine("Error while discovering authority:" + discovery.Error);
+                    Console.ReadKey();
+                    return;
+                }
+
+                var token = await client.RequestPasswordTokenAsync(new PasswordTokenRequest
+                {
+                    ClientId = "test1",
+                    ClientSecret = "test1",
+                    UserName = "gustavo",
+                    Password = "gustavo",
+                    Scope = "ResourceApi",
+                    Address = discovery.TokenEndpoint
+                });
+
+                if (token.IsError)
+                {
+                    Console.WriteLine("Error while loading token: " + token.Error);
+                    Console.ReadKey();
+                    return;
+                }
+
+                Console.WriteLine(token.AccessToken);
+                Console.WriteLine("\n\n");
+
+                client.SetBearerToken(token.AccessToken);
+                var response = await client.GetStringAsync("http://localhost:7000/api/values");
+
+                Console.WriteLine(response);
+
+                Console.ReadKey();
+            }
+        }
+
+        public static async Task GetTokenWithClientCredentials()
+        {
             using (var client = new HttpClient())
             {
                 var discovery = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
